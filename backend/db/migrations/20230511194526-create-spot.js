b57f75d3c85729feb2acf04f84d360a281fbc669
Amanda Morrow <a.holahan7@yahoo.com>
2023-05-18 12:04:14 -0400

    added options to migrations

--- backend/db/migrations/20230511194526-create-spot.js
+++ backend/db/migrations/20230511194526-create-spot.js
@@ -1,8 +1,13 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Spots', {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -60,9 +65,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Spots';
+    return queryInterface.dropTable(options);
   }
 };
