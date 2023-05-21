c3ddcbf6ce2866d38fe9a112f497fa36e6a1c79c
Amanda Morrow <a.holahan7@yahoo.com>
2023-05-21 00:11:22 -0400

    losing hope

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -18,26 +18,26 @@ const spotImageExists = async (req, res, next) => {
     next();
 };
 
-// router.delete('/:imageId', [requireAuth, spotImageExists], async (req, res, next) => {
-//     const { user } = req;
-//     const imageId = req.params.imageId;
-//     const image = await SpotImage.findByPk(imageId);
-//     const spot = await Spot.findOne({
-//         where: {
-//             id: image.spotId
-//         }
-//     });
+router.delete('/:imageId', [requireAuth, spotImageExists], async (req, res, next) => {
+    const { user } = req;
+    const imageId = req.params.imageId;
+    const image = await SpotImage.findByPk(imageId);
+    const spot = await Spot.findOne({
+        where: {
+            id: image.spotId
+        }
+    });
 
-//     if (user.id !== spot.ownerId) {
-//         return res.status(403).json({
-//             message: "Spot must belong to you in order to manipulate it."
-//         });
-//     }
+    if (user.id !== spot.ownerId) {
+        return res.status(403).json({
+            message: "Spot must belong to you in order to manipulate it."
+        });
+    }
 
-//     await image.destroy();
-//     return res.status(200).json({
-//         message: 'Successfully deleted'
-//     });
-// });
+    await image.destroy();
+    return res.status(200).json({
+        message: 'Successfully deleted'
+    });
+});
 
 module.exports = router;
