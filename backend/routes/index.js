bf6e95c02676eb2848af9e35d481422fef572c11
Amanda Morrow <a.holahan7@yahoo.com>
2023-07-28 19:34:24 -0700

    finished phase 0 of frontend authme

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,14 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
