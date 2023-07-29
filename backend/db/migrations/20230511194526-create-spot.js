ec81cd163bc1b289d47addf0be67f6e68b643327
Amanda Morrow <a.holahan7@yahoo.com>
2023-07-28 18:42:22 -0700

    another attempt at numeric overflow bug

--- backend/db/migrations/20230511194526-create-spot.js
+++ backend/db/migrations/20230511194526-create-spot.js
@@ -38,11 +38,11 @@ module.exports = {
         allowNull: false
       },
       lat: {
-        type: Sequelize.DECIMAL(10, 4),
+        type: Sequelize.DECIMAL(9, 4),
         allowNull: false
       },
       lng: {
-        type: Sequelize.DECIMAL(10, 4),
+        type: Sequelize.DECIMAL(9, 4),
         allowNull: false
       },
       name: {
