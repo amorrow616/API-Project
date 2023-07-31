824ade07da557dfae05a06e65047661e69bc7fbb
Amanda Morrow <a.holahan7@yahoo.com>
2023-07-31 10:29:59 -0700

    working on modals from dropdown

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,44 +2,20 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 import "./Navigation.css";
 
 export default function Navigation({ isLoaded }) {
     const sessionUser = useSelector((state) => state.session.user);
-
-    let sessionLinks;
-    if (sessionUser) {
-        sessionLinks = (
-            <li>
-                <ProfileButton user={sessionUser} />
-            </li>
-        );
-    } else {
-        sessionLinks = (
-            <li>
-                <OpenModalButton
-                    buttonText="Log In"
-                    modalComponent={<LoginFormModal />}
-                />
-                <OpenModalButton
-                    buttonText="Sign Up"
-                    modalComponent={<SignupFormModal />}
-                />
-            </li>
-        );
-    }
-
     return (
         <ul>
             <li>
-                <NavLink exact to="/">
-                    Home
-                </NavLink>
+                <NavLink exact to="/"><i class='fa-solid fa-water' />Waterbnb</NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={sessionUser} />
+                </li>
+            )}
         </ul>
     );
 }
--- frontend/src/components/OpenModalButton/index.js
+++ frontend/src/components/OpenModalButton/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useModal } from "../../context/Modal";
 
-function OpenModalButton({
+export default function OpenModalButton({
     modalComponent, // component to render inside the modal
     buttonText, // text of the button that opens the modal
     onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
@@ -16,6 +16,4 @@ function OpenModalButton({
     };
 
     return <button onClick={onClick}>{buttonText}</button>;
-}
-
-export default OpenModalButton;
+};
