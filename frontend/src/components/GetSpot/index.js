0e15dd251108801b4913c18cccd9ead4e84f65b0
Amanda Morrow <a.holahan7@yahoo.com>
2023-08-03 17:53:22 -0700

    css for spot details page set up

--- frontend/src/components/GetSpot/index.js
+++ frontend/src/components/GetSpot/index.js
@@ -20,16 +20,29 @@ export default function GetSpot() {
     }
     return (
         <>
-            <h1 id='spotName'>{spot.name && spot.name}</h1>
-            <div>{spot.city && spot.city}, {spot.state && spot.state}, {spot.country && spot.country}</div>
-            <img src={spot.SpotImages.length && spot.SpotImages[0].url} alt='preview of the spot' id='prevImg'></img>
-            <h2>Hosted by {spot.Owner.firstName && spot.Owner.firstName} {spot.Owner.lastName && spot.Owner.lastName}</h2>
-            <div>{spot.description && spot.description}</div>
-            <div>${spot.price && spot.price} night</div>
-            <div>{spot.avgStarRating && spot.avgStarRating}</div>
-            <div>{spot.numReviews && spot.numReviews} reviews</div>
-            <button onClick={reserveButton}>Reserve</button>
-            <h3>Reviews will go here</h3>
+            <div className='spotPage'>
+                <h1 id='spotName'>{spot.name && spot.name}</h1>
+                <div>{spot.city && spot.city}, {spot.state && spot.state}, {spot.country && spot.country}</div>
+                <div className='detailImages'>
+                    <img src={spot.SpotImages.length && spot.SpotImages[0].url} alt='preview of the spot' id='prevImg'></img>
+                    <img src='https://media.istockphoto.com/id/1263083361/photo/modern-home-with-intricate-detail-throughout.jpg?s=612x612&w=0&k=20&c=Ax4uHREjcKprK5sHPPURkhR-fz4Wct5m-zUoCnOYhiY=' id='detailImage2'></img>
+                    <img src='https://media.istockphoto.com/id/1297586166/photo/modern-elegant-kitchen-stock-photo.jpg?s=612x612&w=0&k=20&c=4qVGJq3EZ-DrVC08kFIXuZMGRe5NcEvziV-H4L9aFKc=' id='detailImage3'></img>
+                    <img src='https://media.istockphoto.com/id/1264323513/photo/scandinavian-bedroom-interior-stock-photo.jpg?s=612x612&w=0&k=20&c=jYMbmLtLDOezPThalkXCfrKjbgkYHpQX2oY3-vjPhVI=' id='detailImage4'></img>
+                    <img src='https://media.istockphoto.com/id/1291917591/photo/modern-bathroom-interior-stock-photo.jpg?s=612x612&w=0&k=20&c=pxqczxYLHDIm0zskG3QgktaO0ICwAd3H4x5b8vdIRuY=' id='detailImage5'></img>
+                </div>
+                <div className='bottomDetail'>
+                    <h2 id='detailName'>Hosted by {spot.Owner.firstName && spot.Owner.firstName} {spot.Owner.lastName && spot.Owner.lastName}</h2>
+                    <div id='detailDescription'>{spot.description && spot.description}</div>
+                    <div className='reserveBox'>
+                        <div id='detailPrice'>${spot.price && spot.price} night</div>
+                        <div id='detailRating'><i class='fa-solid fa-star' />{spot.avgStarRating && spot.avgStarRating}</div>
+                        <div id='detailReviews'>{spot.numReviews && spot.numReviews} reviews</div>
+                        <button onClick={reserveButton} id='detailsButton'>Reserve</button>
+                    </div>
+                </div>
+                <hr />
+                <h3>Reviews will go here</h3>
+            </div>
         </>
     )
 };
