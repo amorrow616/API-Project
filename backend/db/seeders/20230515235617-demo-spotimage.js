881f1bf1250901fb0d9a4b4de6a58b1190b1071c
Amanda Morrow <a.holahan7@yahoo.com>
2023-10-17 20:24:33 -0700

    added images to new spots

--- backend/db/seeders/20230515235617-demo-spotimage.js
+++ backend/db/seeders/20230515235617-demo-spotimage.js
@@ -103,6 +103,156 @@ const spotImages = [
     spotId: 4,
     url: 'https://media.istockphoto.com/id/1263778913/photo/beautiful-basement-entertaining-room-with-led-lighting-in-tray-ceiling.jpg?s=612x612&w=0&k=20&c=iiv2mX5f1WKQspSIr9hxyORZ79MuWwyLDzwdCNWigdw=',
     preview: false
+  },
+  {
+    spotId: 5,
+    url: 'https://media.istockphoto.com/id/136702098/photo/diner-sign-in-red-neon-roadside-restaurant-retro-1950s.jpg?s=612x612&w=0&k=20&c=dVoUCbpdP-IMti0SNAdGhMKVvNvs4ThYazEk3N4Zpf8=',
+    preview: true
+  },
+  {
+    spotId: 5,
+    url: 'https://media.istockphoto.com/id/482307142/photo/diner-restaurant.jpg?s=612x612&w=0&k=20&c=iU-Cz9RvUSA6tguGICbUQoc1F9WZAIB2j2pd7wpEcBY=',
+    preview: false
+  },
+  {
+    spotId: 5,
+    url: 'https://media.istockphoto.com/id/475449558/photo/diner-restaurant.jpg?s=612x612&w=0&k=20&c=8boEdNtqIS3_LI2JRmJkb6hNvK0WpRgMcjXfBL6MFro=',
+    preview: false
+  },
+  {
+    spotId: 5,
+    url: 'https://media.istockphoto.com/id/471386891/photo/ancient-majorcan-bedroom.jpg?s=612x612&w=0&k=20&c=Pz373xnkoW7MCcQJo-F68guAzdIMAelUOGQPhehKVZI=',
+    preview: false
+  },
+  {
+    spotId: 5,
+    url: 'https://media.istockphoto.com/id/1346997224/photo/take-a-bath.jpg?s=612x612&w=0&k=20&c=16HOM2QYkQAjRdG8z1ft7dRkYqhrNmP4GpXXG3sHBA0=',
+    preview: false
+  },
+  {
+    spotId: 6,
+    url: 'https://media.istockphoto.com/id/108219206/photo/log-cabin-hidden-in-the-trees-by-the-lake-ohara-in-canada.jpg?s=612x612&w=0&k=20&c=3rt_fpgh6Be9f2M4GLoGDSteHA4BuEIV9fTiShPJWoM=',
+    preview: true
+  },
+  {
+    spotId: 6,
+    url: 'https://media.istockphoto.com/id/1349844002/photo/cozy-lake-house-living-room-with-lake-view.jpg?s=612x612&w=0&k=20&c=rkKFGwLgIdyi1j-l7MQ0zAtB5bvLBPjpDowUg4-1dXs=',
+    preview: false
+  },
+  {
+    spotId: 6,
+    url: 'https://media.istockphoto.com/id/1269896875/photo/cozy-bedroom-inside-winter-cottage-in-3d-render.jpg?s=612x612&w=0&k=20&c=Dhva-LKdpdh6JJ7fb26G1LRzUG170dGztV11o1xRwEw=',
+    preview: false
+  },
+  {
+    spotId: 6,
+    url: 'https://media.istockphoto.com/id/1270113359/photo/3d-render-of-a-luxurious-interior-of-a-winter-cottage.jpg?s=612x612&w=0&k=20&c=TkBs_-zWRSqPTX_rnvhXIljapFY98KIotxYoGdinEkk=',
+    preview: false
+  },
+  {
+    spotId: 6,
+    url: 'https://media.istockphoto.com/id/157424066/photo/inviting-hot-tub.jpg?s=612x612&w=0&k=20&c=_YB5-PwSKXevpyuK_fIpeUAIZQeU-ZkmOcuvI3bTPlc=',
+    preview: false
+  },
+  {
+    spotId: 7,
+    url: 'https://media.istockphoto.com/id/682432560/photo/stunning-luxury-home-exterior-at-sunset.jpg?s=612x612&w=0&k=20&c=NApF0vAI8wppLkNX73wWRxXUO2nyLHCB6peu38k2rtI=',
+    preview: true
+  },
+  {
+    spotId: 7,
+    url: 'https://media.istockphoto.com/id/1156432390/photo/luxurious-interior-design-of-the-hall-in-a-big-house.jpg?s=612x612&w=0&k=20&c=V0lo1-NdtsuwUU5rynD57RDJqgTjYkbKQooOOExdl24=',
+    preview: false
+  },
+  {
+    spotId: 7,
+    url: 'https://media.istockphoto.com/id/950127464/photo/stunning-kitchen-and-dining-room-in-new-luxury-home-wood-beams-and-elegant-pendant-lights.jpg?s=612x612&w=0&k=20&c=1463LToNVu1M-zOHx5Jiu2ZGnnhntA7sZS3EQTp-khE=',
+    preview: false
+  },
+  {
+    spotId: 7,
+    url: 'https://media.istockphoto.com/id/840452084/photo/spacious-villa-with-cement-wall.jpg?s=612x612&w=0&k=20&c=MqShvppVFL6EfVzybgeOEQxnGQagqyMjhP5ttGwnMO4=',
+    preview: false
+  },
+  {
+    spotId: 7,
+    url: 'https://media.istockphoto.com/id/962109046/photo/spa-with-whirlpool-and-sauna.jpg?s=612x612&w=0&k=20&c=MeurRFz0Q-8qtTzTfTYPs82KCs0Yb6gk-zSjODM0I8o=',
+    preview: false
+  },
+  {
+    spotId: 8,
+    url: 'https://media.istockphoto.com/id/153728492/photo/snowbound-cottage.jpg?s=612x612&w=0&k=20&c=lF8TAILIdt-5JXBCWXfvDE8YsLvxi9G5or9quXJIl8Q=',
+    preview: true
+  },
+  {
+    spotId: 8,
+    url: 'https://media.istockphoto.com/id/928898220/photo/astrology-themed-bedroom-for-kid.jpg?s=612x612&w=0&k=20&c=XxbtbtYFaCn7NxQzwec01lU5SufVvAzuU4AbNT2NZto=',
+    preview: false
+  },
+  {
+    spotId: 8,
+    url: 'https://media.istockphoto.com/id/1058783226/photo/blue-themed-holiday-villa-interior.jpg?s=612x612&w=0&k=20&c=inKh9Rb5jZXEXMyIPLpfP92YIboaTZNxHjAGoxmM3yg=',
+    preview: false
+  },
+  {
+    spotId: 8,
+    url: 'https://media.istockphoto.com/id/989979178/photo/nautical-themed-bathroom-with-a-blue-bath-tub-3d-render.jpg?s=612x612&w=0&k=20&c=0ryobN3AH1ZKvrPiCm42LYlw2m_sQc4hnm3tQ7uHBRk=',
+    preview: false
+  },
+  {
+    spotId: 8,
+    url: 'https://media.istockphoto.com/id/1276730617/photo/nautical-themed-bedroom-with-striped-duvet-on-bed.jpg?s=612x612&w=0&k=20&c=Z6SDDk4YlMT2xALFFVqTx81G9mKERY-P-WWIkpYBFnI=',
+    preview: false
+  },
+  {
+    spotId: 9,
+    url: 'https://media.istockphoto.com/id/1428594094/photo/empty-coffee-shop-interior-with-wooden-tables-coffee-maker-pastries-and-pendant-lights.jpg?s=612x612&w=0&k=20&c=dMqeYCJDs3BeBP_jv93qHRISDt-54895SPoVc6_oJt4=',
+    preview: true
+  },
+  {
+    spotId: 9,
+    url: 'https://media.istockphoto.com/id/1036068800/photo/eco-cotton-linen-and-blanket-on-a-bed-in-nature-loving-family-guesthouse-for-spring-and.jpg?s=612x612&w=0&k=20&c=5gYu66MEXxYz6vnbWimFvaEX-tGZ6lZEJJHjiRElIOU=',
+    preview: false
+  },
+  {
+    spotId: 9,
+    url: 'https://media.istockphoto.com/id/1345839942/photo/ebenalp-switzerland-famous-mountain-guesthouse-aescher-in-the-middle-of-the-hiking-trail.jpg?s=612x612&w=0&k=20&c=NkjO07aD1NGVtrxibnmTtk8feu1BbK69rqUONHJ0wxA=',
+    preview: false
+  },
+  {
+    spotId: 9,
+    url: 'https://media.istockphoto.com/id/1271997900/photo/new-clean-staging-model-home-house-hotel-hostel-or-apartment-guesthouse-shower-bath-bathroom.jpg?s=612x612&w=0&k=20&c=INodZlrVBmDCDVKhUBapK7FUOBwAJqly3jjbCfBsk1Y=',
+    preview: false
+  },
+  {
+    spotId: 9,
+    url: 'https://media.istockphoto.com/id/144726431/photo/classic-interior.jpg?s=612x612&w=0&k=20&c=ZIzZQK3i7CZqZRpWJI9Sr1aNcwFmUU34wy-tQw0zHcs=',
+    preview: false
+  },
+  {
+    spotId: 10,
+    url: 'https://media.istockphoto.com/id/1466527697/photo/view-of-an-abandoned-cabin-in-the-woods.jpg?s=612x612&w=0&k=20&c=gSfKqEPKtRX58gWIBzZfv34Nm2c0qpHMZy8EI2fi1p0=',
+    preview: true
+  },
+  {
+    spotId: 10,
+    url: 'https://media.istockphoto.com/id/1293507877/photo/rotorua-redwoods.jpg?s=612x612&w=0&k=20&c=hRNcDIivxuqWEkfK8-rqrGe0sFPZhoQUBCAZDOv3MU8=',
+    preview: false
+  },
+  {
+    spotId: 10,
+    url: 'https://media.istockphoto.com/id/1162039993/photo/beautiful-asian-woman-nude-in-onsen.jpg?s=612x612&w=0&k=20&c=G0lqoF79ioTwiu9LTJkb5ife7aZRr9Qvxfk4HLY_3xc=',
+    preview: false
+  },
+  {
+    spotId: 10,
+    url: 'https://media.istockphoto.com/id/507832837/photo/chandelier-and-armchair-in-rustic-bedroom.jpg?s=612x612&w=0&k=20&c=eB614nzW0sezhdQ2eeGJGBaajFmI93Pw1ykp5T689vc=',
+    preview: false
+  },
+  {
+    spotId: 10,
+    url: 'https://media.istockphoto.com/id/183373666/photo/modern-and-white-washroom-with-bathtub-and-two-mirrors.jpg?s=612x612&w=0&k=20&c=wIDbxs1Zian_z9MiXETSMwe9I2s23v3xLdxZLuUanSs=',
+    preview: false
   }
 ];
 
