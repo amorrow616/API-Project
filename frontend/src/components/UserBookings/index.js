0365e2fea9feefd999913a934ee5e0c6ebc33a15
Amanda Morrow <a.holahan7@yahoo.com>
2023-10-18 19:26:51 -0700

    read for user bookings complete

---
+++ frontend/src/components/UserBookings/index.js
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import * as bookingActions from '../../store/bookings';
+import './UserBookings.css';
+
+export default function UserBookings() {
+    const dispatch = useDispatch();
+    const bookings = useSelector((state) => state.bookings.user);
+
+    useEffect(() => {
+        dispatch(bookingActions.findUserBookings());
+    }, [dispatch]);
+
+    if (!Object.values(bookings)) return null;
+    return (
+        <>
+            <ul className='userBookingsList'>
+                {Object.values(bookings).map((booking) => (
+                    <li key={booking.id}>
+                        <img className='bookingImage' src={booking.Spot.previewImage && booking.Spot.previewImage} alt='a preview of the spot you have booked' />
+                        <div className='bookingInfo'>
+                            <div>From {booking.startDate.slice(0, 10)} to {booking.endDate.slice(0, 10)}</div>
+                            <div>{booking.Spot.address}</div>
+                            <div>{booking.Spot.city}</div>
+                            <div>{booking.Spot.state}</div>
+                            <div>{booking.Spot.country}</div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
